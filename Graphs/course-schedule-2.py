9095a6bbf9f894577c079d5ab5a353cbf3395b54
Anmol23oct <anmol23oct@ymail.com>
2022-07-04 01:46:41 -0400

    Add files via upload

---
+++ Graphs/course-schedule-2.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        prereq={c:[] for c in range(numCourses)}
+        for crs,pre in prerequisites:
+            prereq[crs].append(pre)
+        
+        output=[]
+        visit,cycle=set(),set()
+        
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            
+            if crs in visit:
+                return True
+            
+            cycle.add(crs)
+            
+            for pre in prereq[crs]:
+                if dfs(pre)==False:
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if dfs(c)==False:
+                return []
+            
+        
+        return output
+        
+        
+        
+        
+        
