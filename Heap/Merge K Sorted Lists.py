babc0cffd93eea006f2fcb9066c5a368e0555e60
Anmol Sharma <anmol23oct@ymail.com>
2022-09-23 21:10:06 -0400

    Add files via upload

---
+++ Heap/Merge K Sorted Lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        
+        head = ListNode(None)
+        curr = head
+        h = []
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+        print(h)
+        while h:
+            val, i = heapq.heappop(h)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if lists[i]:
+                heapq.heappush(h, (lists[i].val, i))
+                lists[i] = lists[i].next
+        
+        return head.next
+    
+        
